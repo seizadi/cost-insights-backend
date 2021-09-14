08322ce300b8754570ac9cf57cd4fe986c93f48e
Soheil Eizadi <seizadi@infoblox.com>
2021-09-13 22:26:45 -0700

    Added Support for Budget

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -20,6 +20,13 @@ type CustomMetric struct {
 	DAC   float64 `json:"DAC"`
 }
 
+type Budget struct {
+	Date   string `json:"Date"`
+	BudgetCAPEX    float64    `json:"BudgetCAPEX"`
+	BudgetOPEX   float64 `json:"BudgetOPEX"`
+	BudgetTotal   float64 `json:"BudgetTotal"`
+}
+
 func getMockMetrics() (*[]CustomMetric, error) {
 	// Open our jsonFile
 	metricsFile, err := os.Open("metrics/metrics.json")
@@ -40,6 +47,28 @@ func getMockMetrics() (*[]CustomMetric, error) {
 	}
 	
 	return &metrics, nil
+}
+
+func getMockBudget() (*[]Budget, error) {
+	// Open our jsonFile
+	budgetFile, err := os.Open("metrics/budget.json")
+	if err != nil {
+		return nil, err
+	}
+	defer budgetFile.Close()
+	
+	byteValue, err := ioutil.ReadAll(budgetFile)
+	if err != nil {
+		return nil, err
+	}
+	
+	var budget []Budget
+	err = json.Unmarshal(byteValue, &budget)
+	if err != nil {
+		return nil, err
+	}
+	
+	return &budget, nil
 }
 
 func getMetricKeyIndex(metrics *[]CustomMetric) map[string]int {
@@ -64,15 +93,49 @@ func getMetricValue(metricType string, curValue float64, curDate string, keys ma
 	return curValue
 }
 
+func getBudgetKeyIndex(budget *[]Budget) map[string]int {
+	keys := make(map[string]int)
+	
+	for index, v := range *budget {
+		keys[v.Date] = index
+	}
+	return keys
+}
+
+func getBudgetValue(budgetType string, curValue float64, curDate string, keys map[string]int, mockBudget *[]Budget) float64 {
+	if index, ok := keys[curDate]; ok {
+		items := *mockBudget
+		item := items[index]
+		if budgetType == "BudgetTotal" {
+			return item.BudgetTotal
+		} else if budgetType == "BudgetCAPEX" {
+			return item.BudgetCAPEX
+		} else if budgetType == "BudgetOPEX" {
+			return item.BudgetOPEX
+		}
+	}
+	return curValue
+}
+
 func GetMetrics(metricType string, intervals string) ([]*pb.DateAggregation, error){
 	retDateAggregation := []*pb.DateAggregation{}
+	var mockMetrics *[]CustomMetric
+	var mockMBudget *[]Budget
+	var keys map[string]int
+	var err error
 	
-	mockMetrics, err := getMockMetrics()
+	if metricType == "BudgetTotal" {
+		mockMBudget, err = getMockBudget()
+		keys = getBudgetKeyIndex(mockMBudget)
+	} else {
+		mockMetrics, err = getMockMetrics()
+		keys = getMetricKeyIndex(mockMetrics)
+	}
 	if err != nil {
 		return retDateAggregation, err
 	}
+
 	
-	keys := getMetricKeyIndex(mockMetrics)
 	
 	r, err := utils.ParseIntervals(intervals)
 	if err != nil {
@@ -115,7 +178,11 @@ func GetMetrics(metricType string, intervals string) ([]*pb.DateAggregation, err
 		}
 		
 		curDate := date.AddDate(0, 0, i).Format(types.DEFAULT_DATE_FORMAT)
-		curValue = getMetricValue(metricType, curValue, curDate, keys, mockMetrics)
+		if metricType == "BudgetTotal" {
+			curValue = getBudgetValue(metricType, curValue, curDate, keys, mockMBudget)
+		} else {
+			curValue = getMetricValue(metricType, curValue, curDate, keys, mockMetrics)
+		}
 		
 		if startValue == 0 && curValue != 0 {
 			startValue = curValue
