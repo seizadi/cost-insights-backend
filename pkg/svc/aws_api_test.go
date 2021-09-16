da4baa20dca5373e58bd524e30777abf8effe6dc
jeizadi <jeizadi@gmail.com>
2021-09-15 21:57:36 -0700

    Added testing for AWS Aggregation

--- pkg/svc/aws_api_test.go
+++ pkg/svc/aws_api_test.go
@@ -35,3 +35,64 @@ func TestGetAwsMetricAmount(t *testing.T) {
 	}
 
 }
+
+func TestAggregationForAws(t *testing.T) {
+	
+	intervals := [][]string {
+		{"2020-08-01", "2020-08-02"},
+		{"2020-08-02", "2020-08-03"},
+		{"2020-08-03", "2020-08-04"},
+	}
+	
+	values := [][]string {
+		{"15.00", "USD"},
+		{"10.00", "USD"},
+		{"100.00", "USD"},
+	}
+	
+	// 9/15/2021 - https://aws.amazon.com/premiumsupport/pricing/
+	// Set the account type to developer then the aggregation should be Greater of $29.00 or 3% added
+	// Set the account type to business then
+	
+	
+	resultByTime := []ceTypes.ResultByTime{
+		{
+			TimePeriod: &ceTypes.DateInterval{
+				Start: &intervals[0][0],
+				End: &intervals[0][1],
+				
+			},
+			Total: map[string]ceTypes.MetricValue{
+				string(ceTypes.MetricNetAmortizedCost) : ceTypes.MetricValue{
+					Amount: &values[0][0],
+					Unit: &values[0][1],
+				},
+			},
+			
+		},
+		{
+			TimePeriod: &ceTypes.DateInterval{
+				Start: &intervals[1][0],
+				End: &intervals[1][1],
+				
+			},
+			Total: map[string]ceTypes.MetricValue{
+				string(ceTypes.MetricNetAmortizedCost) : ceTypes.MetricValue{
+					Amount: &values[1][0],
+					Unit: &values[1][1],
+				},
+			},
+			
+		},
+		
+	}
+	
+		results, err := aggregationForAWS(resultByTime)
+		if err != nil {
+			t.Error("Unexpected error:", err)
+		}
+		if len(results) != len(resultByTime) {
+			t.Errorf("Output %d not equal to expected %d", len(results), len(resultByTime))
+		}
+		
+}
